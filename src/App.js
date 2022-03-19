4571fa793e47430e4a8f49fe0d1a5276ef2849a6
brandodo <brandonong93@gmail.com>
2022-03-19 16:05:29 -0400

    worked on edit warehouse details page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 import React, { Component } from "react";
-// import WarehouseDetails page component
-// import InventoryDetails page component
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "./components/Header.jsx"; // to be added -> Ashley
+import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails.jsx"; // to be added -> Miloan
+import InventoryDetails from "./components/InventoryDetails/InventoryDetails.jsx"; // to be added -> Ashley
+
+import EditWarehouse from "./components/EditWarehouse/EditWarehouse";
 
 export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <Header />
         <Switch>
-          {/* add component/render prop for Warehouse page */}
-          <Route path="/" exact />
-          {/* add component/render prop for Inventory page */}
-          <Route path="/inventory" />
+          {/* make sure component prop matches actual component */}
+          <Route
+            path="/"
+            exact
+            render={(routerProps) => {
+              <WarehouseDetails {...routerProps} />;
+            }}
+          />
+          {/* make sure component prop matches actual component */}
+          <Route
+            path="/inventory"
+            render={(routerProps) => {
+              <InventoryDetails {...routerProps} />;
+            }}
+          />
         </Switch>
       </BrowserRouter>
     );
