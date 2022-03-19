413518281d8c44f79b77c322aeb3efa68e4ac138
brandodo <brandonong93@gmail.com>
2022-03-19 13:34:20 -0400

    added initial routes, need to add route props for each page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React, { Component } from "react";
+// import Warehouse page component
+// import Inventory page component
 
-function App() {
-  return (
-    <div >
-      develop branch!
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          {/* add component/render prop for Warehouse page */}
+          <Route path="/" exact />
+          {/* add component/render prop for Inventory page */}
+          <Route path="/inventory" />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
