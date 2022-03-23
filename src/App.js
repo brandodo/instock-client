0d1ac07b5ca28304ebec418f3989341eca2db7b1
Miloan <miloan27@hotmail.com>
2022-03-22 20:03:24 -0400

    created a Delmodal front-end

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
+import HeroFooter from "./components/HeroFooter/HeroFooter";
+import { BrowserRouter, Switch } from "react-router-dom";
 import React from "react";
 
 import "./App.css";
 
-function App() {
-<<<<<<< HEAD
-	return <div>hello world!</div>;
-=======
-  return (
-    <div >
-      develop branch!
-    </div>
-  );
->>>>>>> develop
+class App extends React.Component {
+	state = {
+		data: null,
+		itemdata: null,
+	};
+
+	onChangeHandler = (data) => {
+		this.setState({
+			data: data,
+		});
+	};
+
+	onChangeHandlerItem = (data) => {
+		this.setState({
+			itemdata: data,
+		});
+	};
+
+	render() {
+		return (
+			<BrowserRouter>
+				<Switch></Switch>
+				<HeroFooter />
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
