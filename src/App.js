39d297fa8c9427e529eac72e2fcf524dd21cf5cd
brandodo <brandonong93@gmail.com>
2022-03-25 01:06:12 -0400

    finalized changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Header from "./components/Header/Header.jsx"; // to be added -> Ashley
+// import Header from "./components/Header/Header.jsx"; // to be added -> Ashley
 import HeroFooter from "./components/HeroFooter/HeroFooter";
 // import WarehouseList from "./components/WarehouseList/WarehouseList.jsx"; // to be added -> Miloan
 // import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails.jsx"; // to be added -> Miloan
@@ -33,7 +33,7 @@ export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Header />
+        {/* <Header /> */}
 
         <Switch>
           {/* <Route
