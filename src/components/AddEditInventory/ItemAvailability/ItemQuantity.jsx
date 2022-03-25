84fe95d1b60ebf829c9564661fa3aec728ac4958
brandodo <brandonong93@gmail.com>
2022-03-24 23:40:29 -0400

    validated changes

--- src/components/AddEditInventory/ItemAvailability/ItemQuantity.jsx
+++ src/components/AddEditInventory/ItemAvailability/ItemQuantity.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ItemQuantity({ value, handleChange }) {
+  const [error, setError] = useState(false);
+
   return (
     <div className="inventoryDetails__itemQuantity">
       <label className="inventoryDetails__label" htmlFor="quantity">
@@ -8,19 +10,27 @@ export default function ItemQuantity({ value, handleChange }) {
       </label>
       <input
         className={`inventoryDetails__textInput ${
-          value === "" ? "inventoryDetails__textInput--error" : ""
+          error ? "inventoryDetails__textInput--error" : ""
         }`}
         type="number"
         name="quantity"
         value={value}
-        onChange={(event) => handleChange(event)}
+        onChange={(event) => {
+          if (event.target.value === "" || event.target.value === "0") {
+            setError(true);
+          } else {
+            setError(false);
+          }
+          handleChange(event);
+        }}
       />
       <div
         className={`inventoryDetails__message ${
-          value === "" ? "inventoryDetails__message--error" : ""
+          error ? "inventoryDetails__message--error" : ""
         }`}
       >
-        This field is required
+        This field is required - quantity cannot be 0, set Status to Out of
+        Stock.
       </div>
     </div>
   );
