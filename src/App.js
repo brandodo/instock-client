f256c24faef3796e8366e569c9eed13dede91bae
Miloan <miloan27@hotmail.com>
2022-03-26 18:45:17 -0400

    warehousedetails and list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-
-
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-// import Header from "./components/Header.jsx"; // to be added -> Ashley
+import Header from "./components/Header.jsx";
 import HeroFooter from "./components/HeroFooter/HeroFooter";
-// import WarehouseList from "./components/WarehouseList/WarehouseList.jsx"; // to be added -> Miloan
-// import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails.jsx"; // to be added -> Miloan
+import WarehouseList from "./components/WarehouseList/WarehouseList.jsx";
+import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails.jsx";
 // import AddEditWarehouse from "./components/AddEditWarehouse/AddEditWarehouse.jsx"; // to be added -> Renish
-
 // import InventoryList from "./components/InventoryList/InventoryList.jsx"; // to be added -> Ashley
 // import InventoryDetails from "./components/InventoryDetails/InventoryDetails.jsx"; // to be added -> Renish
 import AddEditInventory from "./components/AddEditInventory/AddEditInventory.jsx";
 import "./App.scss";
 
 export default class App extends Component {
-  state = {
+	state = {
 		data: null,
 		itemdata: null,
 	};
-  /* Miloan to review if still needed, remove if not needed */
+	/* Miloan to review if still needed, remove if not needed */
 
 	onChangeHandler = (data) => {
 		this.setState({
@@ -31,57 +28,60 @@ export default class App extends Component {
 			itemdata: data,
 		});
 	};
-  
-  render() {
-    return (
-      <BrowserRouter>
-        <Header />
-        
-        <Switch>
-          {/* <Route
-            path="/warehouses"
-            exact
-            render={(routerProps) => <WarehouseList {...routerProps} />}
-          /> */}
 
-          {/* <Route
-            path="/warehouses/:id"
-            render={(routerProps) => <WarehouseDetails {...routerProps} />}
-          /> */}
+	render() {
+		return (
+			<BrowserRouter>
+				<Header />
 
-          {/* <Route
+				<Switch>
+					{
+						<Route
+							path="/warehouses"
+							exact
+							render={(routerProps) => <WarehouseList {...routerProps} />}
+						/>
+					}
+
+					{
+						<Route
+							path="/warehouses/:id"
+							render={(routerProps) => <WarehouseDetails {...routerProps} />}
+						/>
+					}
+
+					{/* <Route
             path="/warehouses/edit/:id"
             render={(routerProps) => <AddEditWarehouse {...routerProps} />}
           /> */}
 
-          {/* <Route
+					{/* <Route
             path="/warehouses/add"
             render={(routerProps) => <AddEditWarehouse {...routerProps} />}
           /> */}
 
-          {/* <Route
+					{/* <Route
             path="/inventory"
             render={(routerProps) => <InventoryList {...routerProps} />}
           /> */}
 
-          {/* <Route
+					{/* <Route
             path="/inventory/:id"
             render={(routerProps) => <InventoryDetails {...routerProps} />}
           /> */}
 
-          <Route
-            path="/inventory/add"
-            render={(routerProps) => <AddEditInventory {...routerProps} />}
-          />
+					<Route
+						path="/inventory/add"
+						render={(routerProps) => <AddEditInventory {...routerProps} />}
+					/>
 
-          <Route
-            path="/inventory/edit/:id"
-            render={(routerProps) => <AddEditInventory {...routerProps} />}
-          />
-        </Switch>
-        <HeroFooter />
-      </BrowserRouter>
-    );
-  }
+					<Route
+						path="/inventory/edit/:id"
+						render={(routerProps) => <AddEditInventory {...routerProps} />}
+					/>
+				</Switch>
+				<HeroFooter />
+			</BrowserRouter>
+		);
+	}
 }
-
